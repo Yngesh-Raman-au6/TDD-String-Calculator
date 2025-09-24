8be95d10c1550dec7bceac5259843796aad72ed9
Yngesh Raman <yngesh.raman@qed42.com>
2025-09-25 00:56:22 +0530

    updating the function to pass the 2 number test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function stringCalculator(string) {
     if (string.length === 1) {
         return parseInt(string);
     }
+    if (string.includes(",")) {
+        return string.split(",").reduce((acc, curr) => acc + parseInt(curr), 0);
+    }
     return 0;
 }   
 
