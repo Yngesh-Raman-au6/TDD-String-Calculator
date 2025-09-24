8b7ea7eb1e80287fad2da2f5b0fec725ca605012
Yngesh Raman <yngesh.raman@qed42.com>
2025-09-25 00:52:01 +0530

    write next test for single number case

--- index.test.js
+++ index.test.js
@@ -4,4 +4,10 @@ describe("stringCalculator", () => {
   it("should return 0 if the string is empty", () => {
     expect(stringCalculator("")).toBe(0);
   });
+
+  it("should return the same number for a single number", () => {
+    expect(stringCalculator("1")).toBe(1);
+  });
+  
+  
 });
