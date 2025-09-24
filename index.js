cc7223427adf89477a2859efc527bc22448d7558
Yngesh Raman <yngesh.raman@qed42.com>
2025-09-25 00:53:44 +0530

    fixing the function to pass the test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function stringCalculator(string) {
-  return 0;
+    if (string === "") {
+        return 0;
+    }
+    if (string.length === 1) {
+        return parseInt(string);
+    }
+    return 0;
 }   
 
 module.exports = { stringCalculator };
